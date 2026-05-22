9da78e69f67f70f44be8d8ee4757e05f11599f9c
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-05-22 12:00:51 +0700

    made new method in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,44 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable prettier/prettier */
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+
 
 @Controller('auth')
 export class AuthController {
 
+  private readonly logger = new Logger(AuthController.name);
   constructor(private authService: AuthService) {}
 
-  @Post('/')
-  async authUser(@Body() loginDto: LoginDto) {
-    return this.authService.login(loginDto.email, loginDto.password);
+
+  //TODO: Implement login endpoint, validate user credentials, and return JWT token.
+
+
+  @Get()
+  async sayHey(){
+    this.logger.log('Hey there! endpoint is working!');
+    return 'Hey there!';
   }
+
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+
+    this.logger.log('Login attempt with email:', loginDto.email);
+
+    
+    const { email, password } = loginDto;
+    return this.authService.validateUser(email, password);
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //TODO: LOGOUT ENDPOINT, invalidate refresh token, and implement token revocation logic.
 
 }
