f4c7c282a8f8cb6cef3e88c303d3f3ddcee06676
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-05-22 11:25:15 +0700

    added database module, user module, and jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,21 +2,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from 'src/users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule, JwtModuleOptions } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
   imports: [
     UsersModule,
+    DatabaseModule,
     
     JwtModule.registerAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService): JwtModuleOptions => ({
-        secret: configService.get<string>("JWT_ACCESS_SECRET"),
+        secret: configService.get<string>('JWT_SECRET'),
         signOptions: { 
-          expiresIn: configService.get<number>('JWT_ACCESS_EXPIRES_IN') 
+          expiresIn: configService.get<number>('JWT_TOKEN_EXPIRES_IN') 
         },
       }),
     }),
