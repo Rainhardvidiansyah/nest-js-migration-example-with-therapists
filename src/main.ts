fb4b3216800a395c0444d57966197f56e42e77d5
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-05-24 09:34:51 +0700

    added app useGlobalPipes in main ts

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import session from 'express-session';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+
+  app.useGlobalPipes(new ValidationPipe({whitelist: true}))
 
   const port = process.env.PORT || 3000;
 
