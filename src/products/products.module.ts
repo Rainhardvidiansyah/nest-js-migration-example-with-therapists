85c21d809782d4bfbbe4896c1e626b6b6d6ef4fd
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-05-30 07:02:47 +0700

    imported database module and redis config module to product module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { productProviders } from './product.providers';
+import { RedisConfigModule } from 'src/redisconfig/redis-config.module';
 
 
 @Module({
- 
 
-  providers: [ProductsService],
+  imports: [DatabaseModule, RedisConfigModule],
  
+  providers: [ProductsService, ...productProviders],
 
   controllers: [ProductsController]
 })
