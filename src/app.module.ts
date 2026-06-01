54e7563ed571a99cb0f42902c3f0a306c575072b
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-06-01 12:49:46 +0700

    imported throttle module to app module ✌️

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TherapistsModule } from './therapists/therapists.module';
 import { DatabaseModule } from './database/database.module';
 import { RolesModule } from './roles/roles.module';
 import { ProductsModule } from './products/products.module';
 import { RedisConfigModule } from './redisconfig/redis-config.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerStorageRedisService } from '@nest-lab/throttler-storage-redis';
 
 
 @Module({
@@ -14,6 +17,22 @@ import { RedisConfigModule } from './redisconfig/redis-config.module';
     DatabaseModule, AuthModule, 
     UsersModule, TherapistsModule,
     RolesModule, ProductsModule, RedisConfigModule,
+
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        throttlers: [{
+          name: 'login',
+          ttl: 60000,
+          limit: 5,
+        }],
+        storage: new ThrottlerStorageRedisService({
+          host: configService.get<string>('REDIS_HOST'),
+          port: configService.get<number>('REDIS_PORT'),
+        }),
+      }),
+    }),
 
     ConfigModule.forRoot({ 
         isGlobal: true,
@@ -24,12 +43,12 @@ import { RedisConfigModule } from './redisconfig/redis-config.module';
         ],
         ignoreEnvFile: false,
       }),
-
-   
-
-    
-
-    
+  ],
+  providers: [
+    {
+      provide: APP_GUARD, 
+      useClass: ThrottlerGuard, // Apply ThrottlerGuard globally
+    },
   ],
 
   
