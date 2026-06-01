07205ed29f90a40627c35749ba274fac1c068379
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-06-01 12:45:57 +0700

    created file response throttle

---
+++ src/common/filters/throttler-exception.filter.ts
@@ -0,0 +1,17 @@
+import { ExceptionFilter, Catch, ArgumentsHost, HttpStatus } from '@nestjs/common';
+import { ThrottlerException } from '@nestjs/throttler';
+import { Response } from 'express';
+
+@Catch(ThrottlerException)
+export class ThrottlerExceptionFilter implements ExceptionFilter {
+  catch(exception: ThrottlerException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    response.status(HttpStatus.TOO_MANY_REQUESTS).json({
+      statusCode: 429,
+      message: 'Too many login attempts. Please try again in 60 seconds.',
+      retryAfter: 60,
+    });
+  }
+}
