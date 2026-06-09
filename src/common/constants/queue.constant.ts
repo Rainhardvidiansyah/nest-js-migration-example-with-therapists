0700a3d77f6d6aa8cda8f3fce50eed4e92ea14db
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-06-09 10:53:00 +0700

    created file constants named queue

---
+++ src/common/constants/queue.constant.ts
@@ -0,0 +1,7 @@
+export const QueueName = {
+  USER_REGISTRATION: 'user-registration',
+  EMAIL_SENDING: 'email-sending',
+  DATA_PROCESSING: 'data-processing',
+  NOTIFICATION: 'notification',
+  REPORT_GENERATION: 'report-generation'
+} as const;
