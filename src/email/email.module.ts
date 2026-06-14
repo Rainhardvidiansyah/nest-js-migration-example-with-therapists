eaa174c15663d5e291fdf988aec69a218de17783
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-06-14 08:54:04 +0700

    deleted redis pub sub from email module and email service

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -3,6 +3,8 @@ import { EmailService } from './email.service';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { RedisConfigModule } from 'src/redisconfig/redis-config.module';
+import { join } from 'node:path';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/adapters/handlebars.adapter';
 
 @Module({
   imports: [
@@ -17,6 +19,11 @@ import { RedisConfigModule } from 'src/redisconfig/redis-config.module';
           //   user: configService.get<string>('EMAIL_USER'),
           //   pass: configService.get<string>('EMAIL_PASSWORD'),
           // },
+        },
+        template: {
+          dir: join(__dirname, 'templates'), // folder tempat welcome.hbs
+          adapter: new HandlebarsAdapter(),
+          options: { strict: true },
         },
         defaults: {
           from: `"No Reply" <${configService.get<string>('EMAIL_FROM')}>`,
--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,38 +1,25 @@
 import { MailerService } from '@nestjs-modules/mailer';
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { RedisChannel } from 'src/common/constants/redis-channel.constants';
-import { RedisPubSubService } from 'src/redisconfig/redis-pubsub.service';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
-export class EmailService implements OnModuleInit{
+export class EmailService{
 
 
   private logger = new Logger(EmailService.name);
 
   constructor(
     private readonly mailService: MailerService,
-    private readonly redisPubSubService: RedisPubSubService
   ){}
 
 
-
-    onModuleInit() {
-      this.redisPubSubService.subscribe(RedisChannel.USER_REGISTERED, (message) => {
-        this.logger.log(`Received message on channel ${RedisChannel.USER_REGISTERED}: ${JSON.stringify(message)}`);
-        this.logger.log(`Sending welcome email to ${message.email}`);
-
-        this.sendEmail(message.email, 'Welcome to Our Service', `Hello, welcome to our service!`);
-      });
-      
-    }
-
-  async sendEmail(to: string, subject: string, message: string): Promise<void>{
+  async sendEmail(to: string, subject: string, context: Record<string, any>): Promise<void>{
 
     try{
       await this.mailService.sendMail({
         to,
         subject,
-        text: message
+        template: 'welcome',
+        context
       });
     } catch (error) {
       if(error instanceof Error){
