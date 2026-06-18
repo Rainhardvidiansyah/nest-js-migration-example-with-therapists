d4ccf4cc4ee6bc9a19511964baade0a597ed040a
Rainhard Vidiansyah <rainhardvidians@gmail.com>
2026-06-18 08:15:37 +0700

    imported customer profiles module to user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,9 +5,10 @@ import { usersProviders } from './users.providers';
 import { UsersController } from './users.controller';
 import { DatabaseModule } from '../database/database.module';
 import { RolesModule } from '../roles/roles.module';
+import { CustomerModule } from 'src/customer/customer.module';
 
 @Module({
-  imports: [DatabaseModule, RolesModule],
+  imports: [DatabaseModule, RolesModule, CustomerModule],
   providers: [UsersService, ...usersProviders],
   exports: [UsersService],
   controllers: [UsersController],
